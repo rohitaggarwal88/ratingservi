e61e22cb72aaba499da2c16b052a8292c842e808
unknown <rohitaggarwal88@yahoo.com>
2025-04-27 20:04:09 +0530

    fourth repo

--- src/main/java/com/rohi/third/rating_data_service/RatingDataServiceApplication.java
+++ src/main/java/com/rohi/third/rating_data_service/RatingDataServiceApplication.java
@@ -2,14 +2,19 @@ package com.rohi.third.rating_data_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
-public class RatingDataServiceApplication {
+public class RatingDataServiceApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RatingDataServiceApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RatingDataServiceApplication.class);
+	}
 }
